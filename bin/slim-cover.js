8afa0a12bfbf035dfb6d57afd66c38aeed696414
Trevor Behlman <tsbehlman@gmail.com>
2017-04-05 19:03:08 -0400

    Remove dependencies on supported test frameworks
    This reduces the size of the module by assuming that the user has
    already installed their test framework either locally or globally, which
    is true in most if not all cases.
    
    The real drawback is that slim-cover doesn't require a particular
    supported version of the test framework, so if a change is made to one
    of them that breaks the hook used by slim-cover, slim-cover would fail
    to cover those tests.

--- bin/slim-cover.js
+++ bin/slim-cover.js
@@ -16,11 +16,11 @@ baseDir = path.resolve( baseDir );
 
 let runners = [
 	{
-		require: "./cover-jasmine.js",
+		module: "jasmine",
 		specDir: "spec"
 	},
 	{
-		require: "./cover-mocha.js",
+		module: "mocha",
 		specDir: "test"
 	}
 ];
@@ -29,8 +29,23 @@ for( let runner of runners ) {
 	let specDir = baseDir + "/" + runner.specDir;
 	let stats = fs.lstatSync( specDir );
 	
-	if( stats.isDirectory() ) {
-		require( runner.require )( baseDir, specDir );
+	if( stats.isDirectory() && hasModule( runner.module ) ) {
+		require( `./cover-${runner.module}.js` )( baseDir, specDir );
 		break;
 	}
+}
+
+const BOLD = "\x1b[1m";
+const RESET = "\x1b[0m";
+
+function hasModule( module ) {
+	try {
+		require.resolve( module );
+	}
+	catch( e ) {
+		console.log( `In order to run these tests you must install ${BOLD}${module}${RESET} via npm` );
+		return false;
+	}
+	
+	return true;
 }
