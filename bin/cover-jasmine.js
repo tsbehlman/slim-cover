169bc74ac9fe52fa7cec38abfbe1b323e60100d8
Trevor Behlman <tsbehlman@gmail.com>
2017-04-05 01:13:21 -0400

    Refactor cli to support runners more modularly

--- bin/cover-jasmine.js
+++ bin/cover-jasmine.js
@@ -1,35 +1,27 @@
-#!/usr/bin/env node
-
-if( process.argv.length < 3 ) {
-	console.log( "Specify a project with tests to execute." );
-	return;
-}
-
-const path = require( "path" );
 const Jasmine = require( "jasmine" );
 const getCoverage = require( "../core" );
 const printCoverage = require( "../shell" );
 
-let baseDir = path.resolve( process.argv[2] );
-let specDir = baseDir + "/spec";
-let configFile = specDir + "/support/jasmine.json";
-
-let jasmine = new Jasmine( {
-	projectBaseDir: baseDir
-} );
-
-jasmine.loadConfigFile( configFile );
-
-Jasmine.prototype.loadSpecs = function() {
-	for( let specFile of this.specFiles ) {
-		getCoverage( specFile );
-	}
+module.exports = function( baseDir, specDir ) {
+	let configFile = specDir + "/support/jasmine.json";
+	
+	let jasmine = new Jasmine( {
+		projectBaseDir: baseDir
+	} );
+	
+	jasmine.loadConfigFile( configFile );
+	
+	Jasmine.prototype.loadSpecs = function() {
+		for( let specFile of this.specFiles ) {
+			getCoverage( specFile );
+		}
+	};
+	
+	jasmine.onComplete( function( passed ) {
+		if( passed ) {
+			printCoverage( __$coverage, process.stdout );
+		}
+	} );
+	
+	jasmine.execute();
 };
-
-jasmine.onComplete( function( passed ) {
-	if( passed ) {
-		printCoverage( __$coverage, process.stdout );
-	}
-} );
-
-jasmine.execute();
---
+++ bin/slim-cover.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+if( process.argv.length < 3 ) {
+	console.log( "Specify a project with tests to execute." );
+	return;
+}
+
+const path = require( "path" );
+const fs = require( "fs" );
+
+let baseDir = path.resolve( process.argv[2] );
+
+let runners = [
+	{
+		require: "./cover-jasmine.js",
+		specDir: "spec"
+	}
+];
+
+for( let runner of runners ) {
+	let specDir = baseDir + "/" + runner.specDir;
+	let stats = fs.lstatSync( specDir );
+	
+	if( stats.isDirectory() ) {
+		require( runner.require )( baseDir, specDir );
+		break;
+	}
+}
